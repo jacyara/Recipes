39c04f24da11de0fbd21f1b91dc2e6a04a253a3f
Jacyara <jacyara@gogood.com.br>
2020-04-10 20:30:42 -0300

    first axios request api

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require('axios');
 const app = express();
 
 app.get('/', function(req, res) {
@@ -7,4 +8,16 @@ app.get('/', function(req, res) {
 
 app.listen(8000, function() {
   console.log('Example app listening on port 8000!');
+});
+
+app.get('/recipes', async function(req, res) {
+  let data;
+  try {
+    data = await axios.get(`http://www.recipepuppy.com/api/?i=onions,garlic,mussels`);
+    console.log(data.data);
+  } catch (error) {
+    console.error(error);
+  }
+  const json = JSON.stringify(data.data);
+  res.send(json);
 });
